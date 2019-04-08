8d173a14ba6866fec982815fde7a469d346a4e1c
Anatoly <AnatolyMuntean@users.noreply.github.com>
2019-04-08 14:21:23 +0300

    Update ForceWorkflowTransition.php

--- ForceWorkflowTransition.php
+++ ForceWorkflowTransition.php
@@ -4,10 +4,20 @@ use Drupal\workflow\Entity\Workflow;
 use Drupal\workflow\Entity\WorkflowManager;
 use Drupal\workflow\Entity\WorkflowTransition;
 
+// $node here is the entity with a workflow field.
+
 /** @var \Drupal\workflow\Entity\Workflow $workflow */
 $workflow = Workflow::load('events_approval');
 /** @var \Drupal\workflow\Entity\WorkflowState $target_state */
 $target_state = $workflow->getState('events_approval_approved');
+
+// Fetch workflow fields attached to a node.
+$workflow_fields = workflow_get_workflow_field_names($node);
+if (empty($workflow_fields)) {
+  return;
+}
+// This one assumes only one workflow field exists.
+$workflow_field = reset($workflow_fields);
 
 $old_sid = WorkflowManager::getPreviousStateId($node, $workflow_field);
 $new_sid = $target_state->id();
